25e6d71f0dea01f28128e27781c76d3529ea2541
Braden O'Guinn <nedarb906@gmail.com>
2013-10-29 11:44:57 -0700

    clean up spacing

--- lib/pig_latin_one.rb
+++ lib/pig_latin_one.rb
@@ -1,3 +1,3 @@
 def pig_latin_one(string)
-  string.downcase.split.map! { |word| (["a", "e", "i", "o", "u"].include?(word[0])) ? word << "way" : word.match(/^[squ]*[qu]*[bcdfghjklmnpqrstvwxyz]*(.+)/)[1] << word.match(/^(squ|qu|[bcdfghjklmnpqrstvwxyz]+)/)[0] << "ay" }.join(" ")
+  string.downcase.split.map! { |word| (%w[a e i o u].include?(word[0])) ? word << "way" : word.match(/^[squ]*[qu]*[bcdfghjklmnpqrstvwxyz]*(.+)/)[1] << word.match(/^(squ|qu|[bcdfghjklmnpqrstvwxyz]+)/)[0] << "ay" }.join(" ")
 end
